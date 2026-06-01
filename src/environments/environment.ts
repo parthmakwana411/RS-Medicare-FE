ffebf5ce1923d5ca471c5c596b32e0527fc27b2a
parthmakwana411 <117499799+parthmakwana411@users.noreply.github.com>
2026-06-01 17:16:19 +0530

    changes URL

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  apiBaseUrl: 'https://radhe-shyam-medical-backend.azurewebsites.net',
-  externalApiBaseUrl: 'https://hischemistapi.ongc.co.in'
+  apiBaseUrl:'https://radhe-shyam-medical-backend.azurewebsites.net',
+  externalApiBaseUrl:'https://hischemistapi.ongc.co.in'
 };
